975e2c029b0568218d57ac37a23cc188fcab91fd
Herbert <herbert.dev.alfa@gmail.com>
2023-03-07 15:34:48 -0300

    00.07 [-g-s]

--- INCLUDES/PAGES/pgINICIAL/pg.inicial.php
+++ INCLUDES/PAGES/pgINICIAL/pg.inicial.php
@@ -2,4 +2,5 @@
 <?php include "INCLUDES/PAGES/pgInicial/ASSETS/div.a.header.html"; ?>
 <?php include "INCLUDES/PAGES/pgInicial/ASSETS/div.searchBar.html"; ?>
 <?php include "INCLUDES/PAGES/pgInicial/ASSETS/table.html"; ?>
+
 </div>
